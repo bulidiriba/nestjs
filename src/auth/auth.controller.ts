aa7a1edceb6326020d02ee14d12394e73414adfa
bulidiriba <bulidiriba@gmail.com>
2022-10-03 17:46:53 +0300

    add initial login and signup endpoints in controller, thats going to be accessed as api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller("auth") // auth is a global route for all the auth endpoints
 export class AuthController {
-    constructor(private authService: AuthService) {} 
+    constructor(private authService: AuthService) {}
+    
+    // define that signup is post request and its specific route is signup, 
+    // so its going to be accessed as localhost:3333/auth/signup
+    @Post("signup")
+    signup() {
+        return "I am signed up"
+    }
+
+    // define that login is post request and its specific route is login, 
+    // so its going to be accessed as localhost:3333/auth/login
+    @Post("login")
+    login() {
+        return "I am login"
+    }
 }
